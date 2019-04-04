9b9a13591d5823c9d8d133ff7124d894cce57e78
Ms2ger <Ms2ger@gmail.com>
2019-04-04 17:28:37 +0200

    Update test for Storage [[Set]].
    See https://github.com/heycam/webidl/pull/706.

--- webstorage/set.window.js
+++ webstorage/set.window.js
@@ -95,18 +95,9 @@
             assert_equals(storage.getItem(key), null);
             assert_equals(storage[key] = value, value);
             // Property is hidden because no [OverrideBuiltins].
-            if (typeof key === "number") {
-                // P is an array index: call through to OrdinarySetWithOwnDescriptor()
-                assert_array_equals(calledSetter, [value]);
-                assert_equals(storage[key], proto);
-                assert_equals(storage.getItem(key), null);
-            } else {
-                // P is not an array index: early return in [[Set]] step 2.
-                // https://github.com/heycam/webidl/issues/630
-                assert_equals(storage[key], proto);
-                assert_equals(Object.getOwnPropertyDescriptor(storage, key), undefined);
-                assert_equals(storage.getItem(key), value);
-            }
+            assert_equals(storage[key], proto);
+            assert_equals(Object.getOwnPropertyDescriptor(storage, key), undefined);
+            assert_equals(storage.getItem(key), value);
         }, "Setting property for key " + key + " on " + name + " with accessor property on prototype");
     });
 });
